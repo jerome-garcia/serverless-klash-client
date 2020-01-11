b5006c0d9a6241e3fd5be07f7db3a79fbf50d795
jerome-garcia <jpgarcia4@up.edu.ph>
2020-01-11 21:11:01 +0800

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "meta-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ng291qghyc.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_cNU7mwqYw",
+    APP_CLIENT_ID: "6cfpdbq3sf0fq61j21kmhhkdd2",
+    IDENTITY_POOL_ID: "us-east-1:8b94f54d-d38f-4f59-8272-e34dc530b5e7"
+  }
+};
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Meta Leagues</h1>
+        <p>Find a match, and climb the boards</p>
+      </div>
+    </div>
+  );
+}

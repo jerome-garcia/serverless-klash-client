296df8c073a137adc0d8af7322bcdc84c17f935a
jerome-garcia <jpgarcia4@up.edu.ph>
2020-01-12 02:19:54 +0800

    Building React app

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import AppliedRoute from "./components/AppliedRoute";
 import NotFound from "./containers/NotFound";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
-export default function Routes() {
+export default function Routes({ appProps }) {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <UnauthenticatedRoute path="/login" exact component={Login} appProps={appProps} />
+      <UnauthenticatedRoute path="/signup" exact component={Signup} appProps={appProps} />
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
     </Switch>
--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ Amplify.configure({
   API: {
     endpoints: [
       {
-        name: "notes",
+        name: "meta",
         endpoint: config.apiGateway.URL,
         region: config.apiGateway.REGION
       },

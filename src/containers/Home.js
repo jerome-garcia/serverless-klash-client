34738f2adac55e61d2d374464215a97d49bbce71
jerome-garcia <jpgarcia4@up.edu.ph>
2020-01-19 20:09:19 +0800

    Minor layout changes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Home.css";
 import MetaTags from 'react-meta-tags';
 import LoaderButton from "../components/LoaderButton";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function Home(props) {
   const [isLoading, setIsLoading] = useState(false);
@@ -30,18 +31,31 @@ export default function Home(props) {
           </MetaTags>
           <div class="container">
             <div class="row">
-              <div class="col-md-4">
+              <div class="col-md-8 col-md-push-4">
+                <img class="img-fluid" src="/home.jpg" alt="Landing-Page"/>
+              </div>
+              <div class="col-md-4 col-md-pull-8">
                 <div class="row">
+                  <br/>
                   <div class="col-md-12">
-                    <h1>Find a match, and climb the boards!</h1>
+                    <h1>Find a match and climb the boards!</h1>
                   </div>
                   <div class="col-md-12">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet consectetur purus. Nulla facilisi. Maecenas quis semper quam. In in facilisis ligula. Proin porta efficitur nibh, vel laoreet diam luctus pharetra. Integer eu magna viverra, maximus elit nec, vestibulum augue. Maecenas efficitur eget nulla imperdiet semper. Donec congue eget est ac condimentum. Integer fermentum congue ultrices. Nullam aliquam semper ligula, in finibus tellus luctus a. Pellentesque sollicitudin sapien diam, a feugiat sem elementum at. Proin at ligula maximus, venenatis mauris quis, tincidunt orci. Sed felis purus, euismod at purus vel, sollicitudin mattis odio.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis, tinciattis odio.</p>
+                    <br/>
+                  </div>  
+                  <div class="col-md-12">
+                    <LinkContainer to="/login">
+                      <LoaderButton block type="submit" bsSize="large" isLoading={isLoading}>
+                        Log In
+                      </LoaderButton>
+                    </LinkContainer>
+                  </div>
+                  <div class="col-md-12">
+                    <h4>Don't have an account? Click here to <a href="/signup">sign up.</a></h4>
+                    <br/>
                   </div>
                 </div>
-              </div>
-              <div class="col-md-8">
-                <img class="img-fluid" src="/home.jpg" alt="Landing-Page"/>
               </div>
             </div>
           </div>
@@ -57,18 +71,26 @@ export default function Home(props) {
         </MetaTags>
         <div className="container">
             <div className="row">
-              <div className="col">
-                <img className="img-fluid" src="/home.jpg" alt="Landing-Page"/>
+              <div class="col-md-12">
+                <div className="col">
+                  <img className="img-fluid" src="/home.jpg" alt="Landing-Page"/>
+                </div>
               </div>
-              <div className="col">
-                <h3>Find a match, and climb the boards!</h3>
-              </div>
-              <div className="col">
-                <form onSubmit={handleSubmit}>
-                  <LoaderButton type="submit" bsSize="large" isLoading={isLoading}>
-                    {isLoading ? "Finding Match" : "Find Match"}
-                  </LoaderButton>
-                </form>
+              <div class="col-md-12">
+                  <div class="col">
+                    <h1>Find a match and climb the boards!</h1>
+                  </div>
+                  <div class="col">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis, tinciattis odio.</p>
+                  </div> 
+                  <br/>
+                  <div className="col">
+                    <form onSubmit={handleSubmit}>
+                      <LoaderButton type="submit" bsSize="large" isLoading={isLoading}>
+                        {isLoading ? "Finding Match" : "Find Match"}
+                      </LoaderButton>
+                    </form>
+                  </div>
               </div>
               <br/>
               <br/>

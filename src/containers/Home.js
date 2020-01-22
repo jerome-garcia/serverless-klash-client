b958e09311f50cdb76a07648eae9c24651ddbec9
jerome-garcia <jpgarcia4@up.edu.ph>
2020-01-22 21:50:34 +0800

    Changes in Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -25,60 +25,41 @@ export default function Home(props) {
 
   function renderLander() {
     return ( props.isAuthenticated ? ( 
-            <div className="row">
-              <div class="col-md-12">
-                <div className="col">
-                  <img className="img-fluid" src="/home.jpg" alt="Landing-Page"/>
-                </div>
-              </div>
-              <div class="col-md-12">
-                  <div class="col">
-                    <h1>Find a match and climb the boards!</h1>
-                  </div>
-                  <div class="col">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis, tinciattis odio.</p>
-                  </div> 
-                  <br/>
-                  <div className="col">
-                    <form onSubmit={handleSubmit}>
-                      <LoaderButton type="submit" bsSize="large" isLoading={isLoading}>
-                        {isLoading ? "Finding Match" : "Find Match"}
-                      </LoaderButton>
-                    </form>
-                  </div>
-                  <br/>
-                  <br/>
-              </div>
-            </div>
+      <div class="row">
+        <br/><br/><br/>
+        <div class="col-md-12">
+          <h1>Find a match and climb the boards!</h1>
+        </div>
+        <br/>
+        <div className="col-md-12">
+          <form onSubmit={handleSubmit}>
+            <LoaderButton block type="submit" bsSize="large" isLoading={isLoading}>
+              {isLoading ? "Finding Match" : "Find Match"}
+            </LoaderButton>
+          </form>
+        </div>
+        <br/>
+        <br/>
+      </div>
     ) : (
-            <div class="row">
-              <div class="col-md-8 col-md-push-4">
-                <img class="img-fluid" src="/home.jpg" alt="Landing-Page"/>
-              </div>
-              <div class="col-md-4 col-md-pull-8">
-                <div class="row">
-                  <br/>
-                  <div class="col-md-12">
-                    <h1>Find a match and climb the boards!</h1>
-                  </div>
-                  <div class="col-md-12">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis, tinciattis odio.</p>
-                    <br/>
-                  </div>  
-                  <div class="col-md-12">
-                    <LinkContainer to="/login">
-                      <LoaderButton block type="submit" bsSize="large" isLoading={isLoading}>
-                        Log In
-                      </LoaderButton>
-                    </LinkContainer>
-                  </div>
-                  <div class="col-md-12">
-                    <h4>Don't have an account? Click here to <a href="/signup">sign up</a>.</h4>
-                    <br/>
-                  </div>
-                </div>
-              </div>
-            </div>
+      <div class="row">
+        <br/>
+        <div class="col-md-12">
+          <h1>Find a match and climb the boards!</h1>
+          <br/>
+        </div>
+        <div class="col-md-12">
+          <LinkContainer to="/login">
+            <LoaderButton block type="submit" bsSize="large" isLoading={isLoading}>
+              Log In
+            </LoaderButton>
+          </LinkContainer>
+        </div>
+        <div class="col-md-12">
+          <h4>Don't have an account? Click here to <a href="/signup">sign up</a>.</h4>
+          <br/>
+        </div>
+       </div>
       )
     );
   }
@@ -86,7 +67,16 @@ export default function Home(props) {
   return (
     <div className="Home">
       <div className="container">
-        {renderLander()}
+        <div className="row">
+          <div class="col-md-8 col-md-push-4">
+            <div className="col">
+              <img className="img-fluid" src="/home.jpg" alt="Landing-Page"/>
+            </div>
+          </div>
+          <div class="col-md-4 col-md-pull-8">
+            {renderLander()}
+          </div>
+        </div>
       </div>
     </div>
   );
